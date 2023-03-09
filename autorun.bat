e0a3f6cc0cf20f0fcc4f5600292a2a4a089a9457
beviltiskas <matasbendikas0@gmail.com>
2023-03-09 16:39:39 +0200

    updated excecution file name in bat.

--- autorun.bat
+++ autorun.bat
@@ -1,7 +1,7 @@
 @echo off
 set "drive=%~d0"
 if not exist "%drive%\autorun.bat" goto end
-PowerShell.exe -ExecutionPolicy Bypass -File "%~dp0\current_pc_data.ps1"
+PowerShell.exe -ExecutionPolicy Bypass -File "%~dp0\scriptv2.ps1"
 pause
 :end
 exit
